b7b84e0871c0e13ebd5dc854f6dd2c1fdcaebb3a
paulgi48 <paulgiovannio@outlook.com>
2023-03-17 12:57:23 +0100

    done

--- components/spinner/spinner.js
+++ components/spinner/spinner.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import spinner from './loading.gif';
 import Image from 'next/image';
 
 const Spinner = () => {
-  return (
-    <>
-      <div className='flex justify-center'>
-        <Image src={spinner} alt='loading' width={200} height={200} />
-      </div>
-    </>
-  );
+  const [showSpinner, setShowSpinner] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSpinner(false), 6500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return showSpinner ? (
+    <div className='flex justify-center'>
+      <Image src={spinner} alt='loading' width={40} height={40} />
+    </div>
+  ) : null;
 };
 
 export default Spinner;
+
